4590320b5139bfefc42178ed6b1cacf4f13dc583
Kevin Lewin <kevin.lewin@abacus.net>
2012-06-14 16:04:58 -0700

    remove original compiled versions, create separate bootstrap folder

--- build.bat
+++ build.bat
@@ -1,2 +1,9 @@
-recess --compile ./less/bootstrap.less > bootstrap.css
-recess --compress ./less/bootstrap.less > bootstrap.min.css
+mkdir -p bootstrap/img
+mkdir -p bootstrap/css
+mkdir -p bootstrap/js
+cp img/* bootstrap/img/
+recess --compile ./less/bootstrap.less > bootstrap/css/bootstrap.css
+recess --compress ./less/bootstrap.less > bootstrap/css/bootstrap.min.css
+recess --compile ./less/responsive.less > bootstrap/css/bootstrap-responsive.css
+recess --compress ./less/responsive.less > bootstrap/css/bootstrap-responsive.min.css
+
